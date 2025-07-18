19eb8526af38fc6976b96ad69289d4a8d6905a85
yuji1123-hyogo <yuji1123200113@gmail.com>
2025-07-19 01:42:47 +0900

    i18nの設定を修正

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module My
 
     config.time_zone = "Tokyo"
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.generators do |g|
