cb526869e3eb1d985ebccb8d965fc149f271e2b8
yuji1123-hyogo <yuji1123200113@gmail.com>
2025-07-19 00:33:47 +0900

    initialcommit

---
+++ config/application.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require_relative "boot"
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module My
+  class Application < Rails::Application
+    config.load_defaults 8.0
+    config.autoload_lib(ignore: %w[assets tasks])
+
+    config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
+    # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+      g.assets false
+      g.helper false
+    end
+  end
+end
