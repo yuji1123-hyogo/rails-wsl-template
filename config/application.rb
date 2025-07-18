b7d5761239d6b6365f268a6a294ff05d001cba45
yuji1123-hyogo <yuji1123200113@gmail.com>
2025-07-19 03:52:55 +0900

    generaterでcontrollerspecが生成されないように変更

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module My
                        view_specs: false,
                        helper_specs: false,
                        routing_specs: false,
-                       controller_specs: true,
+                       controller_specs: false,
                        request_specs: false
       g.fixture_replacement :factory_bot, dir: "spec/factories"
       g.assets false
